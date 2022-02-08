65316ae492a6a413c86e0db6eda6a62b77a5e97a
nopo <noahogno@gmail.com>
2022-02-08 11:08:59 +1100

    dot go brrr

--- Python-Stuff/1.py
+++ Python-Stuff/1.py
@@ -1,1 +1,17 @@
 b = "Hello, World!"
+import time
+
+# import only system from os
+import os
+
+# import sleep to show output for some time period
+from time import sleep
+
+x = 1
+s = "."
+
+while True:
+    print(s)
+    s += "."
+    sleep(1/100)
+    print("\033c")
