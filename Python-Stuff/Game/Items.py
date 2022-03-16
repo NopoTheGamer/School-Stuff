9d3b46cbedbd85a4df834f571cd64e653fd7deba
NopoTheGamer <noahogno@gmail.com>
2022-03-16 21:08:14 +1100

    Added some backpack stuff (100% works)

--- Python-Stuff/Game/Items.py
+++ Python-Stuff/Game/Items.py
@@ -1,13 +1,61 @@
 import Utils
 itemList = ["EMPTY", "nuts", "EMPTY", "EMPTY", "deez"]
 def backpack():
-    print("| You have: ", end="")
+    print("| You have: ", end="| ")
     for a in itemList:
         if a != "EMPTY":
-            print(a, end = " ")
-    Utils.printFormattedLine("Do you want to use an item?")
-    itemInput = input("")
-    for b in itemList:
-        if itemInput.lower() == b.lower():
-            if itemInput != "EMPTY":
-                Utils.printFormattedLine("You used an item !!!!!!")
+            print(a, end = " | ")
+    print()
+    Utils.printFormattedLine("Do you want to use an item? | Yes | No")
+    match input(""):
+        case "yes":
+            Utils.printFormattedLine("What item do you want to use?")
+            itemInput = input("")
+            for b in itemList:
+                if itemInput.lower() == b.lower():
+                    if itemInput != "EMPTY":
+                        Utils.printFormattedLine(f"You used {b} !!!!!!")
+        case _:
+            Utils.printFormattedLine("You didnt use an item")
+def addItem(item):
+    for i in range(len(itemList)):
+        #print(f"{i} {len(itemList)}")
+        if itemList[i] == "EMaPTY":
+            itemList[i] = item
+            break
+        if i == len(itemList) - 1:
+            Utils.printFormattedLine("Your backpack is full bozo")
+            Utils.printFormattedLine("Do you want to replace an item you have?")
+            listItems()
+            Utils.printTrimmer()
+            match input(""):
+                case "yes":
+                    Utils.printTrimmer()
+                    Utils.printFormattedLine("What item do you want to replace?")
+                    Utils.printTrimmer()
+                    userPlayerInputForDroppingInFortnite = input("")
+                    print(itemList)
+                    #for a in range(len(userPlayerInputForDroppingInFortnite)):
+                    #THIS DOESNT WORK YOU FUCKER
+                    #FUCKING STUPID
+                    print("a")
+                    for b in range(len(itemList)):
+                        print("b")
+                        print(f"{userPlayerInputForDroppingInFortnite} + {itemList[b]}")
+                        if userPlayerInputForDroppingInFortnite.lower() == itemList[b].lower():
+                            if userPlayerInputForDroppingInFortnite == "EMPTY":
+                                Utils.printFormattedLine("nice try drongo")
+                            else:
+                                itemList[b] = userPlayerInputForDroppingInFortnite
+                                print(itemList)
+                case _:
+                    Utils.printFormattedLine(f"You just left \"{item}\" on the ground")
+                    Utils.printTrimmer()
+
+
+def listItems():
+    print("| You have: ", end="| ")
+    for a in itemList:
+        if a != "EMPTY":
+            print(a, end=" | ")
+    print()
